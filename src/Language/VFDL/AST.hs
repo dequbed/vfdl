b8cdf04d9694bd1dcf8b87444cf7379191412842
Nadja Reitzenstein <me@dequbed.space>
2020-10-02 22:10:25 +0200

    Structured parser

--- src/Language/VFDL/AST.hs
+++ src/Language/VFDL/AST.hs
@@ -1,25 +1,17 @@
-module Language.VFDL.AST
-    ( AST(..)
-    , PortDirection(..)
-    , Port(..)
-    , Factory(..)
-    , Operation(..)
-    , BOp(..)
-    , Statement(..)
-    , Architecture(..)
-    ) where
+module Language.VFDL.AST where
 
-data AST = AST [Factory] [Architecture]
-    deriving (Eq, Show)
+type Identifier = Text
+type TypeString = Text
 
-instance Semigroup AST where
-    (AST f a) <> (AST g b) = AST (f<>g) (a<>b)
-instance Monoid AST where
-    mempty = AST [] []
+data FactoryDeclaration = FactoryDeclaration
+    { factory_identifier :: Identifier
+    , factory_header :: Maybe PortList
+    } deriving (Eq, Show)
+
+type PortList = [Port]
 
 data PortDirection = In | Out
     deriving (Eq, Show)
-
 
 data Port = Port
     { portIdentifier :: Text
@@ -27,28 +19,62 @@ data Port = Port
     , portType :: Text
     } deriving (Eq, Show)
 
-data Factory = Factory
-    { facIdentifier :: Text
-    , facPorts :: Maybe [Port]
+data ArchitectureBody = ArchitectureBody
+    { arch_identifier :: Identifier
+    , arch_name :: Identifier
+    , arch_declarations :: [BlockDeclaration]
+    , arch_statements :: [ConcurrentStatement]
     } deriving (Eq, Show)
 
-data BOp = Merge
+data BlockDeclaration
+    = BCTransport TransportDeclaration
+    | BCComponent ComponentDeclaration
     deriving (Eq, Show)
 
-data Operation
-    = FunctionCall { name :: Text
-                   , parameter :: [Text]
-                   }
-    | Balance [Text]
-    | BeltOperation BOp Text Text
+data TransportDeclaration = TransportDeclaration
+    { transport_identifier_list :: [Identifier]
+    , transport_type :: ConstructedType
+    } deriving (Eq, Show)
+
+data ConstructedType
+    = Scoped TypeString -- ^ Type valid in the scope it was defined in
+    | Resolved TypeString -- ^ Fully resolved type
     deriving (Eq, Show)
 
-data Statement = Statement
-    { outputs :: [Text]
-    , operation :: Operation
+data ComponentDeclaration = ComponentDeclaration
+    { component_identifier :: Identifier
+    , component_ports :: Maybe PortList
     } deriving (Eq, Show)
 
-data Architecture = Architecture
-    { archIdentifier :: Text
-    , archStatements :: [Statement]
+data Parameter
+    = Quoted Text
+    | Unquoted Identifier
+    deriving (Eq, Show)
+
+data ConcurrentStatement = ConcurrentStatement
+    { cs_target :: [Identifier]
+    , cs_statement :: Statement
     } deriving (Eq, Show)
+
+data Statement
+    = CSProcessCall ProcessCall
+    | CSBalance [Identifier]
+    | CSOperator Operator
+    deriving (Eq, Show)
+
+data ProcessCall = ProcessCall
+    { processName :: Identifier
+    , processParams :: [Parameter]
+    } deriving (Eq, Show)
+
+data Operator
+    = Merge Identifier Identifier
+    deriving (Eq, Show)
+
+data AST = AST [FactoryDeclaration] [ArchitectureBody]
+    deriving (Eq, Show)
+
+instance Semigroup AST where
+    (AST f a) <> (AST g b) = AST (f<>g) (a<>b)
+instance Monoid AST where
+    mempty = AST [] []
